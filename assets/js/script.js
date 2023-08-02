d11f089854b4ea4e53db2f3c412126ce888e1246
Jessie Shockey <shockeyjessie92@gmail.com>
2023-08-02 17:57:27 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+//Define global variables here
+/* 
+One for: Form searchForm
+One for: historicOption
+*/
+
+//storedCities = [];
+
+
+/*function: searchedEventHandler {
+    take the event.target's textContent and assign to variable citySearched
+    call saveCities(citySearched) to update local storage with new city name
+
+    if citySearched is not null then it should trigger the getWeather function
+}*/
+
+/*function: selectedEventHandler {
+    take the event.target's textContent and assign to variable citySelected
+
+    if citySelected is not null then it should trigger the getWeather function
+}*/
+
+/*function: getWeather {
+    take in the city and use that within the fetch 
+
+    within the .then -- call function for currentWeather(data.stuff) and forecastWeather(data.stuff)
+}*/
+
+/* function: currentWeather(city stuff) {
+    take elements from API and createElements
+}*/
+
+/* function: forecastWeather(city stuff) {
+    take elements from API and createElements
+}*/
+
+/* function: saveCities (citySearched){
+    push the citySearched to storedCities
+    send the array to local storage
+}*/
+
+/* function: getCities (citySearched){
+    get data from local storage and insert into citySearched array
+    create a list element for each city in array
+}*/
+
+
+// event listener for page load -- need to pull local storage into saveCities array 
+//searchForm.eventlistener -- function: searchedEventHandler
+//historicOption.eventlistener -- function: selectedEventHandler
